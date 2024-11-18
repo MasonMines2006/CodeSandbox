4e2d436264e16802e6ff53139db0d4830f8f37fc
Mason Mines <mmines@unc.edu>
2024-11-18 17:58:09 -0500

    Created API example in React

--- CSSG Directory/API Example/my-react-app/src/App.jsx
+++ CSSG Directory/API Example/my-react-app/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import movieInfo from "./components/Movie.jsx";
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null); // Declare state for storing fetched data
+
+  function renderMovie(movieInfo) {
+    return <>Movie props = {movieInfo}</>
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjA4NWUwNDExZjAwMTY3NzljN2FmNTU2MTdiNzI4ZiIsIm5iZiI6MTczMTk2MzAyMy45NTI4NzgsInN1YiI6IjY3MzI5MWQ2YmMzZmY3YjRkMWJmMDYzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c50WYwFUp1yJpvaxL6awUJLJ-R7B50OLZdy7aYYVtc8",
+        },
+      };
+
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        console.log(json);
+        setData(json); // Save fetched data to state
+      } catch (err) {
+        console.error(err);
+      }
+      setData(await myResponse.json());
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <>
+        {data.map((movieInfo) => renderMovie())}
+      </>
+  );
 }
 
-export default App
+export default App;
---
+++ CSSG Directory/API Example/my-react-app/src/components/Movie.jsx
@@ -0,0 +1,5 @@
+function movieInfo(movieInfo) {
+    return (
+        <h3>{movieInfo.title}</h3>
+    )
+}
